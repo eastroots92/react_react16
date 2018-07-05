e7a162c0e8a95e4cbfc4cc45545184a3c78996eb
Eastroots92 <eastroots92@gmail.com>
2018-07-05 22:45:46 +0900

    Studing Erro Boundaries
    Error가 났을 경우에 componeeeentDidCatch를 이용해서 에러 캐치를 할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from "react-dom";
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["동근", "인수", "정수", "유정", "슬비", "영화"]
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      })
+    }, 2000);
+  }
+  
+  render() {
+    const { friends } = this.state;
+    return friends.map((friends) => ` ${friends}`);
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(
@@ -18,12 +37,23 @@ class ReturnTypes extends Component {
   }
 }
 
+const ErrorFallback = () => "Sorry It's Error!"
+
 class App extends Component {
+  state = {
+    hasError: false
+  };
+  componentDidCatch(error, info) {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({hasError: true});
+  };
   render() {
+    const { hasError } = this.state;
     return (
       <Fragment>
         <ReturnTypes />
         <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker /> }
       </Fragment>
     );
   }
